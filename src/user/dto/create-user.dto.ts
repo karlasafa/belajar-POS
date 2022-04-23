926d09f55f5512b1d31b55dd5e538bf32ffb2c61
karlasafa <karlasafa20@gmail.com>
2022-04-23 22:42:28 +0700

    tambahan

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,7 +1,22 @@
+import { IsEmail, IsOptional, IsString, maxLength, MaxLength, MinLength } from "class-validator"
 export class CreateUserDto {
+    @IsOptional()
     id : number
+
+    @IsString()
+    @MaxLength(200)
     nama_user : string
+
+    @IsEmail()
     email : string
+
+    @IsString()
+    @MinLength(6)
+    @MaxLength(50)
     username : string
+
+    @IsString()
+    @MinLength(8)
+    @MaxLength(32)
     password : string
 }
